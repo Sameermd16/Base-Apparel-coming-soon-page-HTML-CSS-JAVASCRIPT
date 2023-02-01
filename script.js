1c97cee123d55bd5bc859f2708a7a1b48b9e7d54
sameermd16 <sameerthemc@gmail.com>
2023-02-01 09:01:59 +0530

    script

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const invalid_icon = document.getElementById("invalid_icon");
+const invalid_text = document.getElementById("invalid_text");
+
+// form.addEventListener("submit", (e) => {
+//     if(email.validity.TypeMismatch){
+//         e.preventDefault();
+//         invalid_icon.style.opacity = "1";
+//         invalid_text.style.opacity = "1";
+//     }else{
+//         invalid_icon.style.opacity = "0";
+//         invalid_text.style.opacity = "0";
+//     }
+// })
+
