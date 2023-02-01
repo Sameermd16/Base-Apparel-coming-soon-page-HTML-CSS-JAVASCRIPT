4d49b0e7408b07a6afa168f1640bd58d3badfec9
sameermd16 <sameerthemc@gmail.com>
2023-02-01 14:05:24 +0530

    JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
-const invalid_icon = document.getElementById("invalid_icon");
-const invalid_text = document.getElementById("invalid_text");
+// const invalid_icon = document.getElementById("invalid_icon");
+// const invalid_text = document.getElementById("invalid_text");
 
 // form.addEventListener("submit", (e) => {
 //     if(email.validity.TypeMismatch){
@@ -13,4 +13,17 @@ const invalid_text = document.getElementById("invalid_text");
 //         invalid_text.style.opacity = "0";
 //     }
 // })
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const emailVal = email.value;
+    if (!validateEmail(emailVal)){
+        form.classList.add("error");
+    }else{
+        form.classList.remove("error");
+    }
+})
 
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
